6cd5084200e277d5843df3224d40b0e050969e8e
liuaw <liuaw9087@163.com>
2022-06-19 00:16:45 +0800

    提交number和string相关操作

---
+++ chapter01/code/08-number.ts
@@ -0,0 +1,23 @@
+/* console.log("TypeScript Number 属性: ");
+console.log("最大值为: " + Number.MAX_VALUE);
+console.log("最小值为: " + Number.MIN_VALUE);
+console.log("负无穷大: " + Number.NEGATIVE_INFINITY);
+console.log("正无穷大:" + Number.POSITIVE_INFINITY); */
+/* var month = 0
+if (month <= 0 || month > 12) {
+    month = Number.NaN
+    console.log("月份是：" + month)
+} else {
+    console.log("输入月份数值正确。")
+} */
+/* function employee(id: number, name: string) {
+    this.id = id
+    this.name = name
+}
+
+var emp = new employee(123, "admin")
+employee.prototype.email = "admin@runoob.com"
+
+console.log("员工号: " + emp.id)
+console.log("员工姓名: " + emp.name)
+console.log("员工邮箱: " + emp.email) */
