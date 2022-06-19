d2ca5d8a02be9ed8c14cf4b4fb6f034205e776d1
liuaw <liuaw9087@163.com>
2022-06-19 16:57:33 +0800

    123

---
+++ chapter01/code/09_string.ts
@@ -0,0 +1,9 @@
+/* var txt = new String("string");
+或者更简单方式：
+var txt = "string"; */
+/* var str = new String("This is string");
+console.log("str.constructor is :" + str.constructor) */
+// length 返回字符串的长度
+var uname = new String("Hello World");
+console.log("length:" + uname.length)
+//12
